148706353288167cdd07097a62e07894b9092973
obinoo <nnubiaobinna17@outlook.com>
2024-01-28 00:06:38 +0100

    Updated file

--- src/main/java/com/jumia/FA/Application.java
+++ src/main/java/com/jumia/FA/Application.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.jumia.FA.repository.UserRepository"})
 @ComponentScan(basePackages = {"com.jumia.FA.entity"})
 @OpenAPIDefinition(
 		info = @Info(
