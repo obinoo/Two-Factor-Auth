00c935df73d0502d620546e36429c2c7aa99d442
obinoo <nnubiaobinna17@outlook.com>
2024-02-04 03:33:53 +0100

    Updated file

--- src/main/java/com/jumia/FA/config/Filter.java
+++ src/main/java/com/jumia/FA/config/Filter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,12 +18,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class Filter extends OncePerRequestFilter {
 
-    @Autowired
     private CustomUserDetailsService userDetailsService;
 
-    @Autowired
     private TokenManager tokenManager;
 
         @Override
