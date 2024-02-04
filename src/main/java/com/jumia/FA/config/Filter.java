489e643c564d40588085b08189be6b2e9b52498b
obinoo <nnubiaobinna17@outlook.com>
2024-02-04 03:39:49 +0100

    Updated file

--- src/main/java/com/jumia/FA/config/Filter.java
+++ src/main/java/com/jumia/FA/config/Filter.java
@@ -18,7 +18,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class Filter extends OncePerRequestFilter {
 
     private CustomUserDetailsService userDetailsService;
