002a2d818c3f401d0f445194a79facb42a406856
obinoo <nnubiaobinna17@outlook.com>
2024-02-06 16:01:07 +0100

    Updated file

--- src/main/java/com/jumia/FA/config/CustomUserDetailsService.java
+++ src/main/java/com/jumia/FA/config/CustomUserDetailsService.java
@@ -13,12 +13,8 @@ import java.util.ArrayList;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-   private final UserRepository userRepository;
-
-   @Autowired
-   public CustomUserDetailsService(UserRepository userRepository){
-       this.userRepository=userRepository;
-   }
+    @Autowired
+   private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
