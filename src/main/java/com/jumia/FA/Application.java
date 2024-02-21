12dcbb88fe6bd7ca7fe7f8ae0612e0558e52b3dc
obinoo <nnubiaobinna17@outlook.com>
2024-02-22 00:30:54 +0100

    Updated file

--- src/main/java/com/jumia/FA/Application.java
+++ src/main/java/com/jumia/FA/Application.java
@@ -5,20 +5,14 @@ import com.jumia.FA.service.EmailService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
-import javax.sql.DataSource;
 
 @Configuration
 @SpringBootApplication
@@ -46,5 +40,4 @@ public class Application {
 	public LoginEventListener loginEventListener(EmailService emailService){
 		return new LoginEventListener(emailService);
 	}
-
 }
--- src/main/java/com/jumia/FA/repository/UserRepository.java
+++ src/main/java/com/jumia/FA/repository/UserRepository.java
@@ -4,9 +4,8 @@ import com.jumia.FA.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+    @Repository
+    public interface UserRepository extends JpaRepository<User, Integer> {
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-   User findByEmail(String email);
-}
+        User findByEmail(String email);
+    }
